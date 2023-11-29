e9bfe582c36df00fda4a825c7d31766148275e0d
tehemton2 <152406761+tehemton2@users.noreply.github.com>
2023-11-29 20:25:59 +0530

    Add files via upload

---
+++ src/main/java/poc/tehemton2/App.java
@@ -0,0 +1,14 @@
+package poc.tehemton2;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class App 
+{
+    public static void main( String[] args )
+    {
+        log.info("Hello World!");
+    }
+}
